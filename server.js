1ba60bdb8e3faeb1645c650a3e04369a359f8477
Natalia Chrząścik <56516256+nataliachrzascik@users.noreply.github.com>
2021-08-13 13:14:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -47,9 +47,17 @@ db.mongoose
   res.json({ message: "Workk" });
 });*/
 
+const root = require('path').join(__dirname, 'client', 'build')
+app.use(express.static(root));
+app.get("*", (req, res) => {
+    res.sendFile('index.html', { root });
+})
+
+/*
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname+ '/client/build/index.html'));
 });
+*/
 
 require('./routes/authRoutes.js')(app);
 require('./routes/userRoutes.js')(app);
