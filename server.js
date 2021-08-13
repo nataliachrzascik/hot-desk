f1e2430f6552cbcd882c7a7c9b8e2c0200a53bed
Natalia Chrząścik <56516256+nataliachrzascik@users.noreply.github.com>
2021-08-13 13:30:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ var path = require('path');
 
 const app = express();
 
-var corsOptions = {
+/*var corsOptions = {
   origin: "http://localhost:8081"
 }
+*/
 app.use(cors(corsOptions));
-//app.use(cors());
+app.use(cors({credentials: true, origin: true}));
+
 
 
 
