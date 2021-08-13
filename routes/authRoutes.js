21b697d96b9c12f21ef65de53a99201eb3eb97e8
Natalia Chrząścik <56516256+nataliachrzascik@users.noreply.github.com>
2021-08-13 14:19:06 +0200

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
   });
 
   app.post(
-    "/api/auth/signup",
+    "http://localhost:8080/api/auth/signup",
     [
       verifySignUp.checkDuplicateUsernameOrEmail,
       verifySignUp.checkRolesExisted
