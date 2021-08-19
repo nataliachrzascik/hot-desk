a65aff0c6ca6991b0a54bd524c114b6c4dbc71d5
Natalia Chrząścik <56516256+nataliachrzascik@users.noreply.github.com>
2021-08-19 09:57:03 +0200

    Update dataController.js

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -121,7 +121,7 @@ find('wroclaw2', {'reservations.date' : moment.utc(year+"-"+month+"-"+day), 'pla
    console.log("data.length");
   console.log(data.length);
 
-   if(data!=true){
+  if(data.length===0){
   //zabezpieczenie przed sytuacją, gdy dwóch użytkowników wczytało w tej samej chwili aplikację, i oboje chca zarezerwować ten sam pokój na ten sam dzień
      
 
